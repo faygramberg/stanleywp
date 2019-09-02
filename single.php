11a893e9ca2c2ce5a2e4aeb82599459082a8e7ad
Fay Gramberg <faygramberg@gmail.com>
2019-09-02 12:17:06 +0200

    single blog layout

--- single.php
+++ single.php
@@ -6,27 +6,33 @@
  *
  * @package StanleyWP
  */
-
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	
+		<div id="primary" class="content-area-full">
+			<main id="main" class="site-main" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
+			<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content', get_post_format() ); ?>
 
-			get_template_part( 'template-parts/content', get_post_format() );
+				<div class="container">
+						<div class="row justify-content-center">
+							<div class="col-md-8">
+								<?php the_post_navigation();
+								// If comments are open or we have at least one comment, load up the comment template.
+								if ( comments_open() || get_comments_number() ) :
+									comments_template();
+								endif; ?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+							</div><!--  .col-md-8 -->
+						</div><!--  .row -->
+					</div><!--  .container -->		
 
-		endwhile; // End of the loop.
-		?>
+			<?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</main><!-- #main -->
+		</div><!-- #primary -->
 
 <?php
 get_footer();
