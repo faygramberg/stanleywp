2d125049b2777203c1af7b72bd2e6be663193290
Fay Gramberg <faygramberg@gmail.com>
2019-09-02 11:53:13 +0200

    blog layout

--- index.php
+++ index.php
@@ -11,48 +11,48 @@
  *
  * @package StanleyWP
  */
-
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	
+			<div id="primary" class="content-area-full">
+				<main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) :
+				<?php
+				if ( have_posts() ) :
+					if ( is_home() && ! is_front_page() ) : ?>
+						<header>
+							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+						</header>
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+					<?php
+					endif;
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_format() );
+					endwhile; ?>
+					
+					<div class="container">
+						<div class="row justify-content-center">
+							<div class="col-md-8">
+								<?php the_posts_navigation(); ?>
+							</div><!--  .col-md-8 -->
+						</div><!--  .row -->
+					</div><!--  .container -->
+					
 
-			<?php
-			endif;
+				<?php  
+				else :
+					get_template_part( 'template-parts/content', 'none' );
+				endif; ?>
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-					* Include the Post-Format-specific template for the content.
-					* If you want to override this in a child theme, then include a file
-					* called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					*/
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_pagination( array(
-				'prev_text' => '<i class="fa fa-arrow-left" aria-hidden="true"></i><span class="screen-reader-text">' . __( 'Previous Page', 'pool' ) . '</span>',
-				'next_text' => '<span class="screen-reader-text">' . __( 'Next Page', 'pool' ) . '</span><i class="fa fa-arrow-right" aria-hidden="true"></i>',
-			) ); 
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				</main><!-- #main -->
+			</div><!-- #primary -->
+		
 
 <?php
 get_footer();
--- template-parts/content.php
+++ template-parts/content.php
@@ -6,67 +6,61 @@
  *
  * @package StanleyWP
  */
-
 ?>
 
-<?php
-	$full_img = get_post_meta( get_the_ID(), '_stanleywp_full_featured', true );
-	//echo $full_img;
-?>
+<article id="post-<?php the_ID(); ?>" <?php post_class('blog-post'); ?>>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
 
-	<?php if ( has_post_thumbnail() && is_single() ) : ?>
-		<?php if ($full_img == "") : ?>
-				<div class="post-thumbnail">
-					<?php the_post_thumbnail('full', array('class' => 'rounded')); ?>
-				</div><!--  .post-thumbnail -->
-			<?php else : ?>
-				<div class="post-thumbnail alignfull">
-					<?php the_post_thumbnail('full'); ?>
-				</div><!--  .post-thumbnail -->
-			<?php endif; ?>
-		<?php else : ?>
-			<div class="post-thumbnail">
-		    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-		        <?php the_post_thumbnail('full', array('class' => 'rounded')); ?>
-		    </a>
-		</div><!--  .post-thumbnail -->
-	<?php endif; ?>
+				<?php if ( has_post_thumbnail() && is_single() ) : ?>
+					<div class="post-thumbnail">
+						<?php the_post_thumbnail('full', array('class' => 'rounded')); ?>
+					</div><!--  .post-thumbnail -->
+					<?php else : ?>
+						<div class="post-thumbnail">
+					    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+					        <?php the_post_thumbnail('full', array('class' => 'rounded')); ?>
+					    </a>
+					</div><!--  .post-thumbnail -->
+				<?php endif; ?>
 
-	<header class="entry-header">
-		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif; ?>
-	</header><!-- .entry-header -->
+				<header class="entry-header">
+					<?php
+					if ( is_single() ) :
+						the_title( '<h1 class="entry-title">', '</h1>' );
+					else :
+						the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					endif;
+					if ( 'post' === get_post_type() ) : ?>
+					<div class="entry-meta">
+						<?php stanleywp_posted_on(); ?>
+					</div><!-- .entry-meta -->
+					<?php
+					endif; ?>
+				</header><!-- .entry-header -->
 
-	<?php
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php stanleywp_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	<?php endif; ?>
-	
+				<div class="entry-content">
+					<?php
+						the_content( sprintf(
+							/* translators: %s: Name of current post. */
+							wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'stanleywp' ), array( 'span' => array( 'class' => array() ) ) ),
+							the_title( '<span class="screen-reader-text">"', '"</span>', false )
+						) );
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'stanleywp' ),
+							'after'  => '</div>',
+						) );
+					?>
+				</div><!-- .entry-content -->
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'stanleywp' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+				<footer class="entry-footer">
+					<?php stanleywp_entry_footer(); ?>
+				</footer><!-- .entry-footer -->
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'stanleywp' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+			</div><!--  .col-md-8 -->
+		</div><!--  .row -->
+	</div><!--  .container -->
 
-	<footer class="entry-footer">
-		<?php stanleywp_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
